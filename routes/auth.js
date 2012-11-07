713d41cc0e6f363fbd98410d0ee6edccef310de5
stilefano <stilefano@gmail.com>
2012-11-07 17:20:15 +0100

    navigation is now dynamic

--- routes/auth.js
+++ routes/auth.js
@@ -2,73 +2,84 @@ var mongo = require('mongodb');
 
 var hash = require('./pass').hash;
 
-var Server = mongo.Server,
-    Db = mongo.Db,
-    BSON = mongo.BSONPure;
+var Server = mongo.Server, Db = mongo.Db, BSON = mongo.BSONPure;
 
-var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('authdb', server, {safe: true});
-
-
-db.open(function(err, db) {
-    if(!err) {
-        console.log("Connected to 'authdb' database");
-        db.collection('users', {safe:true}, function(err, collection) {
-            if (err) {
-                console.log("The 'auth' collection doesn't exist. Creating it with sample data...");
-            }
-        });     
-    }
+var server = new Server('localhost', 27017, {
+	auto_reconnect : true
+});
+db = new Db('authdb', server, {
+	safe : true
 });
 
-exports.addUser = function(req,res,fn){
-	
+db.open(function(err, db) {
+	if (!err) {
+		console.log("Connected to 'authdb' database");
+		db.collection('users', {
+			safe : true
+		}, function(err, collection) {
+			if (err) {
+				console.log("The 'auth' collection doesn't exist. Creating it with sample data...");
+			}
+		});
+	}
+});
+
+exports.addUser = function(req, res, fn) {
+
 	var pageParam = req.body;
 
-	function insertInto(pageParam){	
-	    db.collection('users', function(err, collection) {
-	        collection.insert(pageParam, {safe:true}, function(err, result) {
-	            if (err) {					return fn(err.err)
-	            } else {
+	function insertInto(pageParam) {
+		db.collection('users', function(err, collection) {
+			collection.insert(pageParam, {
+				safe : true
+			}, function(err, result) {
+				if (err) {					return fn(err.err)
+				} else {
 					return fn(null)
-	            }
-	        });
-	    });		
+				}
+			});
+		});
 	}
-	
-	hash(req.body.password, function(err, salt, hash){
-		  pageParam.salt = salt;
-		  pageParam.hash = hash;
-		  insertInto(pageParam);
+
+	hash(req.body.password, function(err, salt, hash) {
+		pageParam.salt = salt;
+		pageParam.hash = hash;
+		insertInto(pageParam);
 	})
 }
 
-exports.restrict = function (req, res,username, next) {
-	console.log(req.session.user.username,' ***** ', username)
-  if (req.session.user && req.session.user.username == username) {
-    res.render('restricted',{
-    	message: "Welcome "+username
-    })
-  } else {
-    req.session.error = 'Access denied!';
-    res.redirect('/');
-  }
+exports.restrict = function(req, res, username, next) {
+	if (req.session.user && req.session.user.username == username) {
+		res.render('restricted', {
+			message : "Welcome " + username,
+			isConnected : true
+		})
+	} else {
+		req.session.error = 'Access denied!';
+		res.redirect('/');
+	}
 }
 
 exports.authenticate = function(accounts, pass, fn) {
-  console.log('authenticating %s:%s',accounts, pass)
-  if (!module.parent) console.log('authenticating %s:%s',accounts, pass);   
-  var user = accounts;
-  var userMatched;
-  db.collection('users', function(err, collection) {
-      collection.findOne({'username':user}, function(err, item) {  
-          userMatched=item;
-		  if (!userMatched) return fn(new Error('cannot find user'));
-		  hash(pass, userMatched.salt, function(err, hash){		     
-		    if (err) return fn(err);
-		    if (hash == userMatched.hash) return fn(null, userMatched);
-		    	fn(new Error('invalid password'));
-		  }) 
-      });
-  });  
+	console.log('authenticating %s:%s', accounts, pass)
+	if (!module.parent)
+		console.log('authenticating %s:%s', accounts, pass);
+	var user = accounts;
+	var userMatched;
+	db.collection('users', function(err, collection) {
+		collection.findOne({
+			'username' : user
+		}, function(err, item) {
+			userMatched = item;
+			if (!userMatched)
+				return fn(new Error('cannot find user'));
+			hash(pass, userMatched.salt, function(err, hash) {
+				if (err)
+					return fn(err);
+				if (hash == userMatched.hash)
+					return fn(null, userMatched);
+				fn(new Error('invalid password'));
+			})
+		});
+	});
 }
--- server.js
+++ server.js
@@ -1,127 +1,102 @@
-var express = require('express'),
-	path = require('path'),
-	http = require('http'),
-	url = require('url'),
-	auth = require('./routes/auth');
+var express = require('express'), path = require('path'), http = require('http'), url = require('url'), auth = require('./routes/auth');
 
 var app = express();
 
-app.configure(function(){
-    app.set('port', process.env.PORT || 3000);
-    app.set('host',process.env.HOST || "127.0.0.1");
-    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser()),
-	app.use(express.cookieParser('shhhh, very secret')),
-	app.use(express.session()),
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
+app.configure(function() {
+	app.set('port', process.env.PORT || 3000);
+	app.set('host', process.env.HOST || "127.0.0.1");
+	app.use(express.logger('dev'));
+	/* 'default', 'short', 'tiny', 'dev' */
+	app.use(express.bodyParser()), app.use(express.cookieParser('shhhh, very secret')), app.use(express.session()), app.use(express.static(path.join(__dirname, 'public')));
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
 });
 
-app.use(function(req, res, next){
-  var err = req.session.error
-    , msg = req.session.success;
-  delete req.session.error;
-  delete req.session.success;
-  res.locals.message = '';
-  if (err) res.locals.message = '<p class="msg error">' + err + '</p>';
-  if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
-  res.locals.prova = "..."
-  next();
+app.use(function(req, res, next) {
+	var err = req.session.error, msg = req.session.success;
+	delete req.session.error;
+	delete req.session.success;
+	res.locals.message = '';
+	res.locals.isConnected = false;
+	if (req.session.user)
+		res.locals.isConnected = true;
+	if (err)
+		res.locals.message = '<p class="msg error">' + err + '</p>';
+	if (msg)
+		res.locals.message = '<p class="msg success">' + msg + '</p>';
+	res.locals.prova = "..."
+	next();
 });
 
-app.get("/", function(req,res){
+app.get("/", function(req, res) {
 	res.render('home')
+	console.log("this is the session", req.session, "***************", req.session.user)
 })
 
-app.get("/login", function(req, res){
-	res.render('login.ejs', {
-	});
+app.get("/login", function(req, res) {
+	res.render('login.ejs');
 });
 
-app.get('/signup', function(req,res){
+app.get('/signup', function(req, res) {
 	res.render('signup.ejs')
 })
 
-app.get('/logout', function(req, res){
-  req.session.destroy(function(){
-    res.redirect('/');
-  });
-});
-
-app.get('/restricted?:username',function(req, res,next){
-  var pathname = url.parse(req.url).query;
-  auth.restrict(req,res,pathname,next);
-});
-
-app.post('/signup', function(req,res){
-	auth.addUser(req,res,function(err){
-			if(err){
-				console.log(err)
-					if(err.match('email')){
-						req.session.error = "Error: You can not associate more than one user with the same email"
-					}else if(err.match('username')){
-						req.session.error = "Error: This username already exists"
-					}else{
-						req.session.error = "Error: try again"
-					}
-				res.redirect('/signup')
-			}else{
-			  auth.authenticate(req.body.username, req.body.password, function(err, user){
-			    if (user) {
-			      // Regenerate session when signing in
-			      // to prevent fixation 
-			      req.session.regenerate(function(){
-			        // Store the user's primary key 
-			        // in the session store to be retrieved,
-			        // or in this case the entire user object
-			        req.session.user = user;
-			        req.session.success = 'Authenticated as ' + user.username
-			          + ' click to <a href="/logout">logout</a>. '
-			          + ' You may now access <a href="/restricted?'+user.username+'">restricted</a>.';
-			        res.redirect('/');
-			      });
-			    } else {
-			      req.session.error = 'Authentication failed, please check your user name <br/>'+'<strong>'+err+'</strong>';
-			      res.redirect('login');
-			    }
-			  });
-			}
-		})
+app.get('/logout', function(req, res) {
+	req.session.destroy(function() {
+		res.redirect('/');
 	});
-
-
-app.post('/login', function(req, res){
-  auth.authenticate(req.body.username, req.body.password, function(err, user){
-    if (user) {
-      // Regenerate session when signing in
-      // to prevent fixation 
-      req.session.regenerate(function(){
-        // Store the user's primary key 
-        // in the session store to be retrieved,
-        // or in this case the entire user object
-        req.session.user = user;
-        req.session.success = 'Authenticated as ' + user.username
-          + ' click to <a href="/logout">logout</a>. '
-          + ' You may now access <a href="/restricted?'+user.username+'">restricted</a>.';
-        res.redirect('back');
-      });
-    } else {
-      req.session.error = 'Authentication failed, please check your user name <br/>'+'<strong>'+err+'</strong>';
-      res.redirect('login');
-    }
-  });
 });
 
+app.get('/restricted?:username', function(req, res, next) {
+	var pathname = url.parse(req.url).query;
+	auth.restrict(req, res, pathname, next);
+});
 
+app.post('/signup', function(req, res) {
+	auth.addUser(req, res, function(err) {
+		if (err) {
+			console.log(err)
+			if (err.match('email')) {
+				req.session.error = "Error: You can not associate more than one user with the same email"
+			} else if (err.match('username')) {
+				req.session.error = "Error: This username already exists"
+			} else {
+				req.session.error = "Error: try again"
+			}
+			res.redirect('/signup')
+		} else {
+			auth.authenticate(req.body.username, req.body.password, function(err, user) {
+				if (user) {
+					req.session.regenerate(function() {
+						req.session.user = user;
+						req.session.success = 'Authenticated as ' + user.username + ' click to <a href="/logout">logout</a>. ' + ' You may now access <a href="/restricted?' + user.username + '">restricted</a>.';
+						res.redirect('/');
+					});
+				} else {
+					req.session.error = 'Authentication failed, please check your user name <br/>' + '<strong>' + err + '</strong>';
+					res.redirect('login');
+				}
+			});
+		}
+	})
+});
 
-/*app.get("/",function(req, res){
-  res.locals.session = req.session;
-  res.render('login', { title: 'Login to your account' });
-});*/
+app.post('/login', function(req, res) {
+	auth.authenticate(req.body.username, req.body.password, function(err, user) {
+		if (user) {
+			req.session.regenerate(function() {
+				req.session.user = user;
+				req.session.success = 'Authenticated as ' + user.username + ' click to <a href="/logout">logout</a>. ' + ' You may now access <a href="/restricted?' + user.username + '">restricted</a>.';
+				res.redirect('back');
+			});
+		} else {
+			req.session.error = 'Authentication failed, please check your user name <br/>' + '<strong>' + err + '</strong>';
+			res.redirect('login');
+		}
+	});
+});
 
-http.createServer(app).listen(app.get('port'),function(){
-	console.log("listin on port " + "http://"+app.get('host')+":"+app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+	console.log("listin on port " + "http://" + app.get('host') + ":" + app.get('port'));
 })
-
 
